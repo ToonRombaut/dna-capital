cf396887f69434cf8e8808358c02ab738cdc1c06
Toon Rombaut <toon.rombaut@hotmail.com>
2021-10-17 14:15:38 +0200

    turning dna string with post processing

--- src/assets/shader/fragment.glsl
+++ src/assets/shader/fragment.glsl
@@ -4,8 +4,27 @@ uniform sampler2D texture1;
 uniform vec4 resolution;
 varying vec2 vUv;
 varying vec3 vPosition;
+varying float vColorRandom;
+
+uniform vec3 uColor1;
+uniform vec3 uColor2;
+uniform vec3 uColor3;
 float PI = 3.141592653589793238;
+
 void main()	{
+
+	float alpha = 1. - smoothstep(-0.2,0.5,length(gl_PointCoord - vec2(0.5)));
+	alpha *= 0.5;
+	vec3 finalColor = uColor1;
+	if(vColorRandom > 0.33 && vColorRandom < 0.66){
+		finalColor = uColor2;
+	}
+	if(vColorRandom > 0.66){
+		finalColor = uColor3;
+	}
+
+	float gradient = smoothstep(0.38,0.55,vUv.y);
 	// vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
-	gl_FragColor = vec4(vUv,0.0,1.);
+	gl_FragColor = vec4(finalColor,1.);
+	gl_FragColor = vec4(finalColor,alpha*gradient);
 }
--- src/assets/shader/vertexParticles.glsl
+++ src/assets/shader/vertexParticles.glsl
@@ -1,11 +1,16 @@
 uniform float time;
 varying vec2 vUv;
 varying vec3 vPosition;
+varying float vColorRandom;
 uniform sampler2D texture1;
+
+attribute float randoms;
+attribute float colorRandoms;
 float PI = 3.141592653589793238;
 void main() {
   vUv = uv;
+  vColorRandom = colorRandoms;
   vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
-  gl_PointSize = 1000. * ( 1. / - mvPosition.z );
+  gl_PointSize = (30. * randoms + 10.) * ( 1. / - mvPosition.z );
   gl_Position = projectionMatrix * mvPosition;
 }
